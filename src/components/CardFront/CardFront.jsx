c5aa5975b63c285f13ccc4c03ae8b31a39601295
James Harrison <jamesebharrison@hotmail.com>
2022-03-28 17:46:37 +0100

    compiled files in scss and ran yarn start

--- src/components/CardFront/CardFront.jsx
+++ src/components/CardFront/CardFront.jsx
@@ -6,7 +6,7 @@ import styles from './CardFront.module.scss';
 const CardFront = () => {
     return (
         <div className={styles.cardFront}>
-            <img src="" alt="Thai Green Curry" />
+            <img src="https://www.themealdb.com/images/media/meals/sstssx1487349585.jpg" alt="Thai Green Curry" />
             <section className={styles.recipeDetails}>
                 <h2>Thai Green Curry</h2>
                 <p>
